91f21bb6222718855800267a7b5b454d09105d91
Laurence Tratt <laurie@tratt.net>
2020-02-17 09:54:27 +0000

    -e doesn't make sense unless -o is specified.

--- test.py
+++ test.py
@@ -37,18 +37,9 @@ assert s.stdout.strip() == ""
 
 s = subprocess.run(["./supuner", "-e", "ls", "/bin/sh"],
       capture_output=True, encoding="utf-8")
-assert s.returncode == 0
-assert s.stderr.strip() == ""
-assert s.stdout.strip() == "/bin/sh"
-
-s = subprocess.run(["./supuner", "-e", "ls", "/bin/sh", "/doesntexist"],
-      capture_output=True, encoding="utf-8")
-assert s.returncode != 0
-assert s.stderr.strip() == ""
-assert len(s.stdout.strip().split("\n")) == 2
-for l in s.stdout.strip().split("\n"):
-    l = l.strip()
-    assert_in(l, [RE_BIN_SH, RE_DOESNT_EXIST])
+assert s.returncode == 1
+assert s.stderr.strip() == "-e only allowed if -o is specified"
+assert s.stdout.strip() == ""
 
 with tempfile.NamedTemporaryFile() as tf:
     s = subprocess.run(["./supuner", "-e", "-o", tf.name, "ls", "/bin/sh", "/doesntexist"],
